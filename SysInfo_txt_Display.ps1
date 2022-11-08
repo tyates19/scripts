43b020198f826e80b05c63746be97e56a7642f47
tannerYates <57446951+tyates19@users.noreply.github.com>
2022-11-08 10:12:06 -0500

    Add files via upload

--- SysInfo_txt_Display.ps1
+++ SysInfo_txt_Display.ps1
@@ -1,12 +1,35 @@
 # This script was created on 08/09/2022 by Tanner Yates
 # for the collection of Windows system data.
+#
+# Updated 11/8/2022 to include funny insults on $null user input
+
+$err_mesgs = @(
+			"Hmm, maybe if you speak up, I'll understand...",
+			"INVALID COMMAND!!!  Just kidding...actually not really.  I really don't know what you mean...",
+			"If only I could read minds, then maybe I'd understand...",
+			"DOES YOUR MOTHER KNOW WHAT YOU TYPE WITH THOSE HANDS? ...because I don't.",
+			"I would if I could, but I can't...",
+			"The clouds in the sky are clearer than that.",
+			"HOW DARE YOU?!  I mean, I have no idea what that means, but I'm sure it's terrible.",
+			"DOES NOT COMPUTE!!!",
+			"It can only be attributed to human error.",
+			"That's something I cannot allow to happen.",
+			"Sorry about this, I know it's a bit silly.",
+			"Where did you learn to type?",
+			"My pet ferret can type better than you!",
+			"You type like i drive.",
+			"Do you think like you type?",
+			"Your mind just hasn't been the same since the electro-shock, has it?",
+			"Maybe if you used more than just two fingers...",
+			"I've seen penguins that can type better than that.",
+			"You speak an infinite deal of nothing"
+		)
 
 while (1) {
     $asset = Read-Host -Prompt '
 Enter Asset'
     if ( [string]::IsNullOrEmpty($asset) ) {
-        Write-Output "
-        Uh-oh...blank input not allowed"
+		Write-Output $err_mesgs[$(Get-Random -Maximum $err_mesgs.Count)]
     }
     else {
         break
@@ -21,14 +44,14 @@ $systemInfo_obj = @(
     new-object PSObject -Property @{"Manufacturer" = (Get-WmiObject -Class:Win32_ComputerSystem).Manufacturer; "Model" = (Get-WmiObject -Class:Win32_ComputerSystem).Model; "Hostname" = (Get-WmiObject -Class:Win32_ComputerSystem).Name; "CurrentUser" = (Get-WMIObject -Class:Win32_ComputerSystem).Username; "Domain"  = (Get-WMIObject -Class:Win32_ComputerSystem).Domain; "OS" = (Get-WMIObject -Class:win32_OperatingSystem).Caption; "Version" = (Get-WmiObject -Class:Win32_OperatingSystem).Version; "Serial" = (Get-WmiObject -Class:Win32_Bios).SerialNumber}; Get-NetIPConfiguration | Select-Object @{Name = "Interface"; Expression = {$_.InterfaceAlias}},IPv4Address | Sort-Object Interface | Format-Table; Get-NetAdapter | Select-Object @{Name = "Interface"; Expression = {$_.Name}},MacAddress | Sort-Object Interface | Format-Table
 )
 
-echo "
+Write-Output "
 
 System Info
 ----------"
 
 $systemInfo_obj
 
-write-output "-----BEGIN-----
+Write-Output "-----BEGIN-----
 
 ASSET
 ------
@@ -36,21 +59,23 @@ $asset
 
 " > .\_$asset.txt
 
-write-output "System Info
+Write-Output "System Info
 ----------" >> .\_$asset.txt
 
 $systemInfo_obj >> .\_$asset.txt
 
-echo "------END------" >> .\_$asset.txt
+Write-Output "------END------" >> .\_$asset.txt
 
 sleep 2
 
-write-output "
+Write-Output "
 	...data collected.
 "
 
 pause
 
 $asset = $null
+$err_mesgs = $null
+$systemInfo_obj = $null
 
 Exit
